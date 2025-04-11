1b31f53fc7ab5beafd314651b854e56e235cfa0c
yangzhang1908 <yangzhang1908@gmail.com>
2025-04-11 16:33:47 +0200

    First commit based on Qlicense with migration .net8.0

---
+++ QLicense/LicenseStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLicense
+{
+    public enum LicenseStatus
+    {
+        UNDEFINED = 0,
+        VALID = 1,
+        INVALID = 2,
+        CRACKED = 4
+    }
+}
